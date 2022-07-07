eb5f8b21f066ebee17af9a6f39f9d2d6d91eab6c
Erik Kočamba <445661@mail.muni.cz>
2022-07-07 14:27:30 +0200

    Rename kypo2 to kypo

--- projects/kypo-visualization-dashboard/dashboard/src/dashboard.module.ts
+++ projects/kypo-visualization-dashboard/dashboard/src/dashboard.module.ts
@@ -1,8 +1,8 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
-import { Kypo2TrainingsVisualizationOverviewLibModule } from '@muni-kypo-crp/overview-visualization';
+import { KypoTrainingsVisualizationOverviewLibModule } from '@muni-kypo-crp/overview-visualization';
 import { CommonModule } from '@angular/common';
-import { Kypo2TrainingsHurdlingVizLibModule } from '@muni-kypo-crp/hurdling-visualization';
+import { KypoTrainingsHurdlingVizLibModule } from '@muni-kypo-crp/hurdling-visualization';
 import { SummaryGraphModule } from '@muni-kypo-crp/command-visualizations/summary-graph';
 import { ReferenceGraphModule } from '@muni-kypo-crp/command-visualizations/reference-graph';
 import { DashboardMaterialModule } from './dashboard-material.module';
@@ -16,8 +16,8 @@ import { VisualizationDashboardConfig } from '@muni-kypo-crp/visualization-dashb
   ],
   imports: [
     CommonModule,
-    Kypo2TrainingsVisualizationOverviewLibModule,
-    Kypo2TrainingsHurdlingVizLibModule,
+    KypoTrainingsVisualizationOverviewLibModule,
+    KypoTrainingsHurdlingVizLibModule,
     SummaryGraphModule,
     ReferenceGraphModule,
     DashboardMaterialModule,
@@ -43,8 +43,8 @@ export class DashboardModule {
         SummaryGraphModule.forRoot(config).providers,
         SummaryGraphModule.forRoot(config).providers,
         ReferenceGraphModule.forRoot(config).providers,
-        Kypo2TrainingsVisualizationOverviewLibModule.forRoot(visualizationConfig).providers,
-        Kypo2TrainingsHurdlingVizLibModule.forRoot(visualizationConfig).providers,
+        KypoTrainingsVisualizationOverviewLibModule.forRoot(visualizationConfig).providers,
+        KypoTrainingsHurdlingVizLibModule.forRoot(visualizationConfig).providers,
         {
           provide: VisualizationDashboardConfig,
           useValue: config,
